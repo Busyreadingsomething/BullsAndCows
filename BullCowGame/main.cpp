06690e65082032c37193d465a16296b5d194bca6
Makan Azarshahy <oro1919@gmail.com>
2018-09-07 11:31:58 -0700

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Makan Azarshahy on 9/7/18.
+//  Copyright © 2018 Makan Azarshahy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
