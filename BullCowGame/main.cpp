13df99d91b470e82183537d6ea05a487b250e211
Makan Azarshahy <oro1919@gmail.com>
2018-09-08 14:37:09 -0700

    Added user input

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -7,13 +7,25 @@
 //
 
 #include <iostream>
+//#include <string>
 
 using namespace std;
 
 int main() {
-	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	constexpr int WORD_LENGTH = 9;
+	
+	// introduce the game
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	
+	// get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+	
+	// repeat the guess back to the player
+	cout << "Your guess was: " << Guess << "\n";
 	return 0;
 }
 
