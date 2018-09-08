4e2130f15f2e11aaae3d64a2379806bf8caa6049
Makan Azarshahy <oro1919@gmail.com>
2018-09-08 14:19:28 -0700

    Added the WORD_LENGTH to the cout line

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main() {
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-	cout << "Can you guess the X letter isogram I'm thinking of?" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
 	return 0;
 }
 
