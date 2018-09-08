2140a7c5b4826bb12ede5ea0ef8833d1eea68b08
Makan Azarshahy <oro1919@gmail.com>
2018-09-08 15:01:34 -0700

    Separated the intro into a new function called PrintIntro

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -11,18 +11,21 @@
 
 using namespace std;
 
-int main() {
-	constexpr int WORD_LENGTH = 9;
-	
+void printIntro() {
 	// introduce the game
+	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 	cout << endl;
-	
+	return;
+}
+
+int main() {
+	printIntro();
 	// get a guess from the player
 	string Guess = "";
 	cout << "Enter your guess: ";
-	cin >> Guess;
+	getline(cin, Guess);
 	
 	// repeat the guess back to the player
 	cout << "Your guess was: " << Guess << "\n";
